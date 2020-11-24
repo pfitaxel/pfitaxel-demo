82905744cdb86873f52ff446a7bcffc0341e303f
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-11-24 19:52:07 +0100

    Build learn-ocaml (ocamlsf/learn-ocaml:master)

---
+++ docs/js/learnocaml-config.js
@@ -0,0 +1,8 @@
+var learnocaml_config = {
+  enableTryocaml: true,
+  enablePlayground: true,
+  enableLessons: true,
+  enableExercises: true,
+  enableToplevel: true,
+  root: "https://pfitaxel.github.io/pfitaxel-demo"
+}
